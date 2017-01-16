7d41b4f8b5c93021cba06d1444395ee60e095e6b
raychan <raycha0505@gmail.com>
2017-01-15 20:09:38 -0500

    371_SumOfTwoIntegers

---
+++ Easy/371_SumOfTwoIntegers.js
@@ -0,0 +1,13 @@
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+//
+// Example:
+//   Given a = 1 and b = 2, return 3.
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+  return b === 0 ? a : getSum(a ^ b, (a & b) << 1);
+}
